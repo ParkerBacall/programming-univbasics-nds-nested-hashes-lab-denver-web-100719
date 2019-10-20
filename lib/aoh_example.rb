e4a6ea1db199cf2471168dcd1b6ccc0884037081
Parker Bacall <pbacall2@gmail.com>
2019-10-20 19:34:22 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -12,11 +12,14 @@ def assembled_aoh
 end
 
 def literal_aoh
-  literal_array = [{ name:  "Don Gately", occupation: "Live-in Staff" },
+  literal_array = [
+  { name:  "Don Gately", occupation: "Live-in Staff" },
   { name:  "Joelle van Dyne", occupation: "Radio Personality" },
-  { name:  "Pat Monteseian", occupation: "Staff" }.
+  { name:  "Pat Monteseian", occupation: "Staff" },
   { name:  "Kate Gompert", occupation: "None" },
-  { name:  "Bruce Green", occupation: "Fan of Mildred" }]
+  { name:  "Bruce Green", occupation: "Fan of Mildred" }
+]
+  
   literal_array
 end
 
