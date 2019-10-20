b23f87bd16fef85c887c44fb45205df984ac3649
Parker Bacall <pbacall2@gmail.com>
2019-10-20 19:33:11 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -13,7 +13,11 @@ end
 
 def literal_aoh
   literal_array = [{ name:  "Don Gately", occupation: "Live-in Staff" },
-  { name:  "Joelle van Dyne", occupation: "Radio Personality" }]
+  { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+  { name:  "Pat Monteseian", occupation: "Staff" }
+  { name:  "Kate Gompert", occupation: "None" }
+
+]
 end
 
 def aoh_lookup(aoh, row, key)
